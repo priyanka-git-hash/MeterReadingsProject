4b97a773f6207d8873c14c0f8eee810c89c7201a
Priyanka Pokharna <priyanka.pokharna@gmail.com>
2025-07-31 00:16:50 +0100

    chore: add EF Core migration and update DB

---
+++ MeterReadingsApi/Migrations/20250730231604_InitialCreate.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MeterReadingsApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    AccountId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.AccountId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MeterReadings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AccountId = table.Column<int>(type: "INTEGER", nullable: false),
+                    MeterReadingDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    MeterReadValue = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MeterReadings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MeterReadings_Accounts_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "AccountId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Accounts",
+                columns: new[] { "AccountId", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1234, "Freya", "Test" },
+                    { 1239, "Noddy", "Test" },
+                    { 1240, "Archie", "Test" },
+                    { 1241, "Lara", "Test" },
+                    { 1242, "Tim", "Test" },
+                    { 1243, "Graham", "Test" },
+                    { 1244, "Tony", "Test" },
+                    { 1245, "Neville", "Test" },
+                    { 1246, "Jo", "Test" },
+                    { 1247, "Jim", "Test" },
+                    { 1248, "Pam", "Test" },
+                    { 2233, "Barry", "Test" },
+                    { 2344, "Tommy", "Test" },
+                    { 2345, "Jerry", "Test" },
+                    { 2346, "Ollie", "Test" },
+                    { 2347, "Tara", "Test" },
+                    { 2348, "Tammy", "Test" },
+                    { 2349, "Simon", "Test" },
+                    { 2350, "Colin", "Test" },
+                    { 2351, "Gladys", "Test" },
+                    { 2352, "Greg", "Test" },
+                    { 2353, "Tony", "Test" },
+                    { 2355, "Arthur", "Test" },
+                    { 2356, "Craig", "Test" },
+                    { 4534, "JOSH", "TEST" },
+                    { 6776, "Laura", "Test" },
+                    { 8766, "Sally", "Test" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MeterReadings_AccountId",
+                table: "MeterReadings",
+                column: "AccountId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MeterReadings");
+
+            migrationBuilder.DropTable(
+                name: "Accounts");
+        }
+    }
+}
