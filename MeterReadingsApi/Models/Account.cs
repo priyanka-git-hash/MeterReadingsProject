0d3c8a0be6d1dd66ac4c428c01634974b64c8a7e
Priyanka Pokharna <priyanka.pokharna@gmail.com>
2025-07-30 23:49:16 +0100

    chore: add .gitignore and Account/MeterReading model classes

---
+++ MeterReadingsApi/Models/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace MeterReadingsApi.Models
+{
+    public class Account
+    {
+        public int AccountId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ MeterReadingsApi/Models/MeterReading.cs
@@ -0,0 +1,11 @@
+﻿namespace MeterReadingsApi.Models
+{
+    public class MeterReading
+    {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public DateTime MeterReadingDateTime { get; set; }
+        public int MeterReadValue { get; set; }
+        public Account Account { get; set; }
+    }
+}
